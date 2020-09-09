e0ac11e7555407366e34dab4f0488641daf09a5e
Stefan Müller <mueller@badw.de>
2020-09-09 20:47:36 +0200

    Linkliste: Öffnen in neuem Fenster; rel='noopener noreferrer'

--- Leseansichtsliste_schreiben.py
+++ Leseansichtsliste_schreiben.py
@@ -3,7 +3,7 @@ from urllib.parse import quote
 
 urpath = os.path.abspath('P-htm')
 url_form = 'https://gitlab.lrz.de/badw-data/mhd-korpus/-/raw/arbeitsfassung/P-htm/'
-listenlink_form = '- <a href="https://publikationen.badw.de/de/data?url={}">{}</a>\n'
+listenlink_form = '- <a href="https://publikationen.badw.de/de/data?url={}" target="_blank" rel="noopener noreferrer">{}</a>\n'
 with open('Leseansichten.md', 'w', encoding = 'utf-8') as file:
     file.write('# Verlinkungen von Leseansichten\n\n')
     for dirpath, subdirnames, subfilenames in os.walk(urpath):
